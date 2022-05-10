a3e186b47a5cc040608967d6008b514c291ffa9b
Archanagowda23 <archanar249@gmail.com>
2022-05-10 22:39:34 +0530

    Added message

--- src/com/bridgelabz/addressbookjsoncsv/Main.java
+++ src/com/bridgelabz/addressbookjsoncsv/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("welcome");
         System.out.println("added");
+        System.out.println("message");
     }
 	}
